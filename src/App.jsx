1db298fff6096018b0d60e599a77e9da5b8d61c4
Udeme98 <Darealudeme1@gmail.com>
2024-04-17 09:30:19 +0100

    installed react-router-dom library

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import HomeCards from "./components/HomeCards";
