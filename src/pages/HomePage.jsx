432b447728e31130dc45f23fe6410b65b9aabe07
Udeme98 <Darealudeme1@gmail.com>
2024-04-18 06:41:39 +0100

    HomePage setup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewJobs from "../components/ViewJobs";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewJobs />
     </>
   );
 };
