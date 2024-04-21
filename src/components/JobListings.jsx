ad7c33b82d2dc7ce4939b5642f9223f07234abbd
Udeme98 <Darealudeme1@gmail.com>
2024-04-21 09:21:36 +0100

    handling errors using try-catch

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,9 +10,15 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const res = await fetch("https://localhost:8000/jobs");
-      const data = await res.json();
-      setJobs(data);
+      try {
+        const res = await fetch("https://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchJobs();
   }, []);
