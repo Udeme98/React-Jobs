9d743ffb8337f1bdd482b5b22db5d196f98127d6
Udeme98 <Darealudeme1@gmail.com>
2024-04-22 13:27:23 +0100

    use state to fetch each job

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
 const JobPage = () => {
-  return <div>JobPage</div>;
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+        // console.log(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJob();
+  }, []);
+  return loading ? <Spinner /> : <h2>{job.title}</h2>;
 };
 export default JobPage;
