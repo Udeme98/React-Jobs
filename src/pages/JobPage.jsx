e22dad913e29cf20597b9934a17b96238968b1a6
Udeme98 <Darealudeme1@gmail.com>
2024-04-22 13:53:07 +0100

    refactored the code to fetch data using DataLoader instead of useEffect hook

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import { useParams, useLoaderData } from "react-router-dom";
 
 const JobPage = () => {
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // Fetching data using React-router DataLoader
   const { id } = useParams();
+  const job = useLoaderData();
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-        // console.log(data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchJob();
-  }, []);
-  return loading ? <Spinner /> : <h2>{job.title}</h2>;
+  return <h2>{job.title}</h2>;
 };
-export default JobPage;
+
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
+
+// using state to manage data fetching:
+
+// import { useEffect, useState } from "react";
+// import Spinner from "../components/Spinner";
+
+//  const [job, setJob] = useState(null);
+//  const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchJob = async () => {
+//       try {
+//         const res = await fetch(`/api/jobs/${id}`);
+//         const data = await res.json();
+//         setJob(data);
+//         // console.log(data);
+//       } catch (error) {
+//         console.log("Error fetching data", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     fetchJob();
+//   }, []);
