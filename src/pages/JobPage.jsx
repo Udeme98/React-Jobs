48f7bb78f95e8e32dfba60a9e8579cd3f4b85629
Udeme98 <Darealudeme1@gmail.com>
2024-04-23 12:19:29 +0100

    job page complete

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,5 @@
 import { useParams, useLoaderData, Link } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 
 const jobLoader = async ({ params }) => {
   const res = await fetch(`/api/jobs/${params.id}`);
@@ -10,6 +10,8 @@ const JobPage = () => {
   // Fetching data using React-router DataLoader
   const { id } = useParams();
   const job = useLoaderData();
+
+  const { name, description, contactEmail, contactPhone } = job.company;
 
   return (
     <>
@@ -32,7 +34,7 @@ const JobPage = () => {
                 <div className="text-gray-500 mb-4">{job.type}</div>
                 <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                  <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+                  <FaMapMarker className="text-orange-700 mr-1" />
                   <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
@@ -58,22 +60,22 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md">
                 <h3 className="text-xl font-bold mb-6">Company Info</h3>
 
-                <h2 className="text-2xl">{job.company.name}</h2>
+                <h2 className="text-2xl">{name}</h2>
 
-                <p className="my-2">{job.company.description}</p>
+                <p className="my-2">{description}</p>
 
                 <hr className="my-4" />
 
                 <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {job.company.contactEmail}
+                  {contactEmail}
                 </p>
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {job.company.contactPhone}
+                  {contactPhone}
                 </p>
               </div>
 
@@ -81,7 +83,7 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to="/add-job.html"
+                  to={`/jobs/edit/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
