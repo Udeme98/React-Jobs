f88616d995b70644a44db7fc6625c84472376ac6
Udeme98 <Darealudeme1@gmail.com>
2024-04-24 09:36:45 +0100

    created a function to post jobs and pass the function as props

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,17 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
+const addJob = (newJob) => {
+  console.log(newJob);
+};
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
       <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="/add-job" element={<AddJobPage />} />
+      <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
