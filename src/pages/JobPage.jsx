d2d13e1dd777940f2f6adefb10aa560dd8982108
Udeme98 <Darealudeme1@gmail.com>
2024-04-25 08:25:04 +0100

    installed react-toastify: notified job has been added successfully

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,6 @@
 import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const jobLoader = async ({ params }) => {
   const res = await fetch(`/api/jobs/${params.id}`);
