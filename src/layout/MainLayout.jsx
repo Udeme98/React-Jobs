64c82729b56167ffcef30899fd10bdb0b5a53b1b
Udeme98 <Darealudeme1@gmail.com>
2024-04-26 12:53:18 +0100

    footer section

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
       <ToastContainer />
     </>
   );
