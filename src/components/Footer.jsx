f72d7e13093df1a11e148dd24493aa1bf75f9ea8
Udeme98 <Darealudeme1@gmail.com>
2024-04-27 12:45:22 +0100

    footer section: added the logo and react icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,23 @@
+import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaTwitter, FaYoutube, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-indigo-700">
-      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0 md:items-center">
         <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
           <div className="mx-auto my-6 text-center text-white md:hidden">
-            Copyright &copy; 2022, All Rights Reserved.
+            Copyright &copy; 2024, All Rights Reserved.
           </div>
           <div>
-            <img src="img/logo-white.svg" className="h-8" />
+            <img className="h-10 w-auto mb-5" src={logo} alt="React Jobs" />
           </div>
           <div className="flex justify-center space-x-4">
-            <Link to="#">
-              <img src="img/icon-facebook.svg" />
-            </Link>
-            <Link to="#">
-              <img src="img/icon-youtube.svg" />
-            </Link>
-            <Link to="#">
-              <img src="img/icon-twitter.svg" />
-            </Link>
-            <Link to="#">
-              <img src="img/icon-pinterest.svg" />
-            </Link>
-            <Link to="#">
-              <img src="img/icon-instagram.svg" />
-            </Link>
+            <FaYoutube className="inline-block text-3xl text-white" />
+            <FaLinkedin className="inline-block text-3xl text-white" />
+            <FaTwitter className="inline-block text-3xl text-white" />
+            <FaFacebook className="inline-block text-3xl text-white" />
           </div>
         </div>
 
@@ -48,7 +39,7 @@ const Footer = () => {
         </div>
 
         <div className="hidden text-white md:block">
-          Copyright &copy; 2023. All Right Reserved
+          Copyright &copy; 2024. All Right Reserved
         </div>
       </div>
     </footer>
